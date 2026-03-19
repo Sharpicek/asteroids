dc59f2b1f3995dbbd7a39b0efa9148d5a4db4776
Sharpicek <davidthomas13@proton.me>
2026-03-19 13:46:05 +0100

    Create infinite game loop and game screen
    In main.py I created working infinite gameloop and working black gamescreen, that you can turn off by pressing a 'x' button. This was part of a Gameloop/Game Loop lesson.

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
 
 def main():
+    # Initialize pygame
+    pygame.init()
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # Setting up the screen
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Infinite Game Loop
+    while True:
+        log_state()
+        
+        for event in pygame.event.get():
+            pass
+        
+        screen.fill("black")
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
 if __name__ == "__main__":
     main()
