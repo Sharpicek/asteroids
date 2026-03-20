c3f155b8c2859761fc5f5ccd90c5578c76317571
Sharpicek <davidthomas13@proton.me>
2026-03-20 09:49:42 +0100

    Create player/asteroid collision detection
    Created collision detection between player and asteroid objects. Lesson Asteroids/Collisions.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import pygame
+import pygame, sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
@@ -36,6 +36,13 @@ def main():
                 return
         
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game Over!")
+                sys.exit()
+                
         screen.fill("black")
 
         for shape in drawable:
