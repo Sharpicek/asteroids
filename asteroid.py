a274fed67215dc2f0b1e95dcf0f3bcc39c0b2cb5
Sharpicek <davidthomas13@proton.me>
2026-03-20 12:15:01 +0100

    Add spliting of asteroids
    Asteroids will split in two after being shot and get smaller. Small asteroids will disappear. Lesson Asteroids/Splitting

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,7 @@
-import pygame
+import pygame, random
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +12,17 @@ class Asteroid(CircleShape):
     
     def update(self, dt):
         self.position += self.velocity * dt 
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        random_angle = random.uniform(20, 50)
+        velocity_a = self.velocity.rotate(random_angle)
+        velocity_b = self.velocity.rotate(-random_angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid_a = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_b = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_a.velocity = velocity_a * 1.2
+        asteroid_b.velocity = velocity_b * 1.2
